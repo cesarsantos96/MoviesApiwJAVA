a9fab96a62d68963486c598b6e1d75f0161f33db
Cesar Santos <csar.santos18@gmail.com>
2023-03-28 13:51:56 -0500

    api-movies JAVA

--- api-stickers/src/App.java
+++ api-stickers/src/App.java
@@ -3,10 +3,11 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
+import java.util.List;
+import java.util.Map;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
 
         // fazer uma conexão HTTP e buscar o top 250 filmes.
         String url = "https://imdb-api.com/en/API/Top250Movies/k_04g1zatj";
@@ -17,7 +18,9 @@ public class App {
         String body = response.body();
         System.out.println(body);
 
-        // pegar só os dados que interessam (Título, poster e classificação).
+        // extrair só os dados que interessam (Título, poster e classificação)
+        var parser = new JsonParser();
+        List<Map<String, String>> listaDeFilmes = parser.parse(body);
 
         // Exibir e manipular os dados.
     }
